6e145b97ac2825e4f38e72430b011d8a5ebe9cd9
Cyril Van Dyke <cvandyke@navexglobal.com>
2018-06-19 15:46:56 -0700

    Minimum Viable Product

---
+++ player-selector/README.txt
@@ -0,0 +1,4 @@
+This is a small python program to make switching names on ports easier when streaming Smash Bros
+
+Current implementation is hard-coded to the group that plays at lunch :)
+Future improvements include a variable playerlist, blank slots, and an .exe release
---
+++ player-selector/player-selector.py
@@ -0,0 +1,67 @@
+# structure.py
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class PlayerSelection(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+
+        layout = QGridLayout()
+
+        self.label = QLabel("Player/Port Selection")
+        self.playerList = ["Andrew","Cyril","Flank","Grant","Shane","Weston"]        
+        self.p1_group = QButtonGroup(self)
+        self.p2_group = QButtonGroup(self)
+        self.p3_group = QButtonGroup(self)
+        self.p4_group = QButtonGroup(self)
+        self.groupList = [self.p1_group,self.p2_group,self.p3_group,self.p4_group]
+
+        p1label = QLabel("Player 1")
+        p2label = QLabel("Player 2")
+        p3label = QLabel("Player 3")
+        p4label = QLabel("Player 4")
+
+        for i in range(0,4):
+            for j in range(len(self.playerList)):
+                var = QRadioButton(self.playerList[j])
+                self.groupList[i].addButton(var)
+                layout.addWidget(var, 2+j, i)
+                
+        layout.addWidget(self.label, 0,0)
+        layout.addWidget(p1label, 1,0)
+        layout.addWidget(p2label, 1,1)
+        layout.addWidget(p3label, 1,2)
+        layout.addWidget(p4label, 1,3)
+
+        self.p1_group.buttonClicked['QAbstractButton *'].connect(self.button_clicked1)
+        self.p2_group.buttonClicked['QAbstractButton *'].connect(self.button_clicked2)
+        self.p3_group.buttonClicked['QAbstractButton *'].connect(self.button_clicked3)
+        self.p4_group.buttonClicked['QAbstractButton *'].connect(self.button_clicked4)
+        self.setLayout(layout)
+
+    def button_clicked(self, button, fileName):
+        print(button.text())
+        f = open(fileName,'w')
+        f.write(button.text())
+        f.close()
+
+    def button_clicked1(self, button):
+        self.button_clicked(button,"p1.txt")
+    def button_clicked2(self, button):
+        self.button_clicked(button,"p2.txt")
+    def button_clicked3(self, button):
+        self.button_clicked(button,"p3.txt")
+    def button_clicked4(self, button):
+        self.button_clicked(button,"p4.txt")
+
+       
+
+
+app = QApplication(sys.argv)
+dialog = PlayerSelection()
+dialog.show()
+app.exec_()
